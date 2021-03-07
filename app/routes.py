16521b096f59247db5b2af569bba332684f3af9f
Mike <magutierrezsistemas@gmail.com>
2021-03-07 19:08:38 +0100

    Front ready

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    hosts = [
+            {'IP': 'Hosts example 0'},
+            {'IP': 'Hosts example 1'},
+            {'IP': 'Hosts example 2'},
+            {'IP': 'Hosts example 3'}
+            ]
+    return render_template('index.html', title='Monitoring Network', hosts=hosts)
